0bd82ce20564460943cfd61a2de557b9cff212e7
codedearta <sepp.renfer@gmail.com>
2017-06-30 22:40:21 +0100

    excercise_1

---
+++ excercise_1.js
@@ -0,0 +1,18 @@
+// Um eine constante zu definieren benutzen wir das Schuesselwort 'const'
+const name = "Nyla";
+
+// Um eine variable zu definierne benutzen wir das Schluesselwort 'let'
+let alterInTagen = 14;
+
+/* Um global eine variable zu definiern benutzen wir das Schluesselwort var.
+   In der alten Javascript version war dies die einzige moeglichkeit variablen
+   zu definieren. const und let war nicht verfuegbar. Dies ist der Grund wesshalb
+   viele beispiele immer noch 'var' verwenden. Dies gilt aber heute als sehr schlechte
+   Praxis und sollte nicht mehr verwendet werden. */
+var globalerNonsens = "Donald Trump";
+
+/* Alle Ausdruecke sollte man mit einem Semikolon ';' beenden. Dies ist streng
+   genommen nicht notwendig gilt aber als guter Stil und sollte desshal immer
+   angewendet werden. */
+
+/* Mehrzeilige Kommentare oeffnet man mit '/*' und schliesst man mit Start slash */
