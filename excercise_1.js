1b19e804bb288640d617e206d996554851ffeb40
codedearta <sepp.renfer@gmail.com>
2017-06-30 22:44:09 +0100

    arrays

--- excercise_1.js
+++ excercise_1.js
@@ -1,8 +1,12 @@
 // Um eine constante zu definieren benutzen wir das Schuesselwort 'const'
-const name = "Nyla";
+const name = 'Nyla';
+// Konstanten koennen nicht veraendert werden.
+// name = 'My-Yen' // spukt einen Fehler aus.
 
 // Um eine variable zu definierne benutzen wir das Schluesselwort 'let'
 let alterInTagen = 14;
+alterInTagen = 15; // ist korrekt und funktioniert einwandfrei.
+
 
 /* Um global eine variable zu definiern benutzen wir das Schluesselwort var.
    In der alten Javascript version war dies die einzige moeglichkeit variablen
@@ -16,3 +20,15 @@ var globalerNonsens = "Donald Trump";
    angewendet werden. */
 
 /* Mehrzeilige Kommentare oeffnet man mit '/*' und schliesst man mit Start slash */
+
+// arrays in javascript sind sehr einfach wir definierne sie mit square brackets
+const namen = ['Nyla', 'My-Yen', 'Sepp'];
+
+// um alle namen zu printen keonnen wir einen for-loop verwenden.
+for (let i = 0; i < namen.length; i++) {
+    console.log(namen[i]);
+}
+
+// Das ist ein bisschen old-school. In modenernem Javascript koennen wir die array-funktion forEach verwenden
+namen.forEach(name => cosnole.log(name));
+// hier wird eine lambda funktion verwendet 'name => console.log(name)' mehr dazu spaeter.
