7f62e15caad0fc1ad81e4cd8e99d34b02737fedf
codedearta <sepp.renfer@gmail.com>
2017-07-05 22:04:02 +0100

    additional explanations for if, while, do-while and object literals

--- excercise_1.js
+++ excercise_1.js
@@ -38,3 +38,60 @@ for (let i = 0; i < namen.length; i++) {
 // Das ist ein bisschen old-school. In modenernem Javascript koennen wir die array-funktion forEach verwenden
 namen.forEach(name => cosnole.log(name));
 // hier wird eine lambda funktion verwendet 'name => console.log(name)' mehr dazu spaeter.
+
+// if else construct
+if(globalerNonsens === 'Donald Trump') { // Um Werte zu vergleichen wird in Javascript das dreifache gleicheheitszeichen verwendet.
+  console.log(`Americas president is ${globalerNonsens}`);
+  // rueckwaertsgerichtete anfuefrungszeichen werden fuer template strings verwendet.
+  // mit ${variablennamen} wird die platzhalter variable definiert
+} else {
+  console.log('The World is alright :-)');
+}
+
+// if kann auch ganz alleine stehen
+if(globalerNonsens === 'Donald Trump'){
+  console.log(`Americas president is ${globalerNonsens}`);
+}
+
+let namenTest = ['My-Yen', 'Nyla', 'Sepp'];
+if(namenTest[0] === 'Nyla') {
+  console.log('Nyla is always first.');
+} else if(namenTest[0] === 'My-Yen') {
+  // else if kommt nach id un kann beliebig oft wiederholt werden
+  console.log(`${namenTest[0]} is always first.`);
+} else {
+  // else kommt am Schluss, muss aber nicht zwingend definiert werden
+  console.log('Sepp is probabely first.');
+}
+
+// while loop
+let actualName = ''
+while(actualName !== 'Sepp') {  // ungleichheit wird in Javascript mit !== geprueft.
+  actualName = namenTest.shift(); // shift() entfernt das erste element von einem array un gibt den wert zurueck.
+  console.log('actual name is: ' + actualName);
+}
+
+// do-while loop
+let count = 10;
+do {
+  console.log(`countdown is: ${count}`)
+  count--;
+} while(count > 0);
+
+// Objekte werden mit geschweiften Klammern definiert. Es koennen Werte und Funktionen auf Objekten definert werden.
+const baby = {
+  firstName : 'Nyla',
+  middleName: 'Josephine',
+  lastName: 'Renfer',
+  birthday: new Date(2017, 5, 17), // monat wir doferweise null basiert angegeben
+  parents: ['My-Yen', 'Sepp'],
+  printParents: function() {
+    this.parents.forEach(p => console.log(p));
+  }
+}
+
+
+console.log(baby);
+console.log(`firstName is ${baby.firstName}`);
+console.log(baby.middleName);
+baby2.printParents();
