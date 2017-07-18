e5ce73b180e7563be4da4c7fcf1e1f730a654772
My-Yen Ngo <myyen.ngo@gmail.com>
2017-07-18 23:04:56 +0100

    komment

--- excercise_1.js
+++ excercise_1.js
@@ -107,8 +107,8 @@ const add = function (a, b) {
 
 // aufgerufen (oder funktion ausfuehren) wird die Funktion folgendermassen
 const result = add(1,2);
-console.log('result is:', result); //console log nimmt mehrere Pararmeter und printet alle aus.
-console.log(`result is: ${result}`); // gleich wie obenb aber nur andere syntax
+console.log('result is:', result); //console log nimmt mehrere Pararmeter und printet alle aus. String zusammensetzen mit , nur im console.log
+console.log(`result is: ${result}`); // gleich wie oben aber nur andere syntax
 
 // oder direkt ausdrucken
 console.log('result is:', add(4,5)); // oder ohne var. resulte var.uebergabe und direkt ausdrucken
@@ -130,11 +130,14 @@ const divide = (a, b) => a / b; // => ersetzt  function...{return...}
 // Art 3 als Lambda multiline expression
 const divideSave = (a, b) => {
   if(b === 0) {
-    throw 'division by zero'
-  } else if(Number.isFinite(a) && Number.isFinite(b)) {
+    throw 'division by zero'; // throw ruft einen Fehler hervor und bricht ab. Kommentare kommt auch
+  } else if(Number.isFinite(a) && Number.isFinite(b))  /* Number ist ein Objekt wie console.
+                                                    Number.isFinite(a) ueberprueft ob a eine endliche numer ist.
+                                                    && ist mathematische Verknuepfung */
+  {
     return a / b;
   } else {
-    throw 'can''t devide ' + a + ' and ' + b
+    throw 'can\'t devide ' + a + ' and ' + b; // string zusammensetzen mit +
   }
 }
 
@@ -142,13 +145,27 @@ const divideSave = (a, b) => {
 const divideResult = divide(2,5);
 console.log('divide result is:', divideResult);
 
-console.log(divideSave('hallo', 0));
+console.log(divideSave('hallo', 0)); // fehlermeldung, geht nicht
 
 // Lambdas sind praktisch wenn man sie inline verwendet, sie koennen anderen Funktionen ubergeben werden.
-const delegatorPrint = function(lambda) {
-  return console.log(lambda());
+const delegatorPrint = function(lambda) { // lamda ist hier der selbergewaehlte name, die funktion ist leer
+  console.log(lambda());            /* diese zwei Zeilen ist nur definition:
+                                    die funktion uebernimmt eine funktion und print die aus. wird ausgefuehrt wenn aufgerufen */
 }
 
-delegatorPrint(() => 'ich bin eine Lambdafunktion ;-)');
+delegatorPrint(() => 'ich bin eine Lambdafunktion ;-)'); // aufruf der funktion. keine feste zuweisung, wird nur einmal ausgefuehrt
 
-delegatorPrint(() => 'Pi ist: ' + Math.PI);
+delegatorPrint(() => 'Pi ist: ' + Math.PI); // Math ist ein Hilfsobjekt fuer math. Konstante und Funktionen
+// Math.PI ist eine fixe Konstante und keine funktion, deshalb keine ()
+
+
+const buchstabenArray = 'Hallo My-Yen'.split(''); // gibt ['H', 'a'...]
+const woerterArray = 'Hallo My-Yen'.split(' '); // gibt ['Hallo','My-Yen']
+const listenArray = '1,2,3,4,5,6,Banane'.split(',') // gibt
+const listenArray2 = '1;2;3;4;5;6;Banane'.split(';') // gibt
+
+
+// join()
+
+const einString = ['H','a','l','l','o'].join();
+const einAndererString = ['H','a','l','l','o'].join(',');
